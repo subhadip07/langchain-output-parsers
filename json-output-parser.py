63008e2336f168b1ff476efa79b57c97a91f549c
subhadip07 <subhadipbag18@outlook.com>
2025-07-27 12:01:23 +0530

    added JSON output parser code

---
+++ json-output-parser.py
@@ -0,0 +1,22 @@
+from langchain_groq import ChatGroq
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
+
+load_dotenv()
+
+model = ChatGroq(model='llama-3.1-8b-instant')
+
+parser = JsonOutputParser()
+
+template = PromptTemplate(
+    template='Give me 5 facts about {topic} \n {format_instruction}',
+    input_variables=['topic'],
+    partial_variables={'format_instruction': parser.get_format_instructions()}
+)
+
+chain = template | model | parser
+
+result = chain.invoke({'topic':'virat kohli'})
+
+print(result)
