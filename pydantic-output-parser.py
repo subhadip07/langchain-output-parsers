92b22d392f1cabef1810fd315f65d339d7680276
subhadip07 <subhadipbag18@outlook.com>
2025-07-27 12:12:02 +0530

    added pydantic output parser code

---
+++ pydantic-output-parser.py
@@ -0,0 +1,29 @@
+from langchain_groq import ChatGroq
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
+from pydantic import BaseModel, Field
+
+load_dotenv()
+
+model = ChatGroq(model='llama-3.1-8b-instant')
+
+class Demo(BaseModel):
+    name: str = Field(description='Name of the person')
+    age: int = Field(gt=18, description='Age of the person')
+    city: str = Field(description='Name of the city the person belongs to')
+
+
+parser = PydanticOutputParser(pydantic_object=Demo)
+
+template = PromptTemplate(
+    template='Generate the name, age and city of a fictional {place} person \n {format_instruction}',
+    input_variables=['place'],
+    partial_variables={'format_instruction':parser.get_format_instructions()}
+)
+
+chain = template | model | parser
+
+final_result = chain.invoke({'place':'Indian'})
+
+print(final_result)
